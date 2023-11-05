a4721b795bfbb29e1e6cd510a32fc0477fa3f18e
AlexEatsBurgers <125730192+AlexEatsBurgers@users.noreply.github.com>
2023-11-05 22:14:08 +1100

    installed & completed sendgrid config

--- app/api/mail/route.js
+++ app/api/mail/route.js
@@ -1,11 +1,41 @@
-export async function GET(req, res) {
+const sgMail = require('@sendgrid/mail')
 
-   return new Response(JSON.stringify({status: "ok"}))
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+export async function GET(req, res) {
+   return new Response(JSON.stringify({ status: "ok" }))
 }
 
 export async function POST(req, res) {
-    const body = await req.json()
+   const body = await req.json()
    console.log(body)
 
-   return new Response(JSON.stringify({status: "ok"}))
+   const message = `
+    Name: ${body.name}<br><br>
+    Email: ${body.email}<br><br>
+    Message: ${body.message}
+  `;
+
+   const data = {
+      to: 'sarah.m.manssour@gmail.com', // Change to your recipient
+      from: 'support@aprentz.com', // Change to your verified sender
+      subject: 'Test successful!',
+      text: message,
+      html: `<strong>${message}</strong>`,
+   }
+   try {
+      await sgMail.send(data)
+         .then(() => {
+            console.log('Email sent')
+         })
+   } catch (error) {
+      console.error(error)
+   }
+
+
+
+
+
+
+return new Response(JSON.stringify({ status: "ok" }))
 }
